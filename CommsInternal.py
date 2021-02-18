154ddd49e82de286e463ef54f21bf18bd9e3730a
tatayu <e0323328@u.nus.edu>
2021-02-18 23:38:30 +0800

    send without ack

--- CommsInternal.py
+++ CommsInternal.py
@@ -1,5 +1,6 @@
 from bluepy import btle
 import struct
+import time
 
 BEETLEMAC1 = '80:30:DC:D9:23:1E'
 
@@ -9,17 +10,26 @@ class Delegate(btle.DefaultDelegate):
     
     def handleNotification(self, cHandle, data):
         global receivedData
-        if(bytes(data) == b'A'): #handshake packet
+        global missPakcet
+        global receivedPacket
+        if(data == b'A'): #handshake packet
             handShakeFlag[BEETLEMAC1]= True
+
+        elif (b'}' in data): #detect the end of a packet
+            try:
+                receivedData += data[0:data.index(b'}')+1]
+                unpackedData = struct.unpack('<c6Hc', receivedData)
+                receivedPacket += 1
+                #print(unpackedData)
+                receivedData = bytes() #reset
+                receivedData += data[data.index(b'}')+1:len(data)]
         
-        elif ('}' in data.decode('ISO-8859-1')): #detect the end of a packet
-            receivedData += data
-            charac= beetle.getCharacteristics(uuid = 'dfb1')[0]
-            charac.write(bytes('A', 'ISO-8859-1'), withResponse=False)
-            unpackedData = struct.unpack('<cc6Hc', receivedData)
-            print(unpackedData)
-            receivedData = bytes() #reset
-        
+            except Exception as e:
+                missPakcet += 1
+                print(missPakcet)
+                receivedData = bytes()
+                receivedData += data[data.index(b'}')+1:len(data)]
+
         else:
             receivedData += data
 
@@ -32,9 +42,16 @@ def handShake(beetle):
         charac.write(bytes('A', 'ISO-8859-1'), withResponse=False)
 
 def getData(beetle):
+    global receivedPacket
+    start = time.time()
     while(1):
-        beetle.waitForNotifications(2)       
-    
+        beetle.waitForNotifications(2)
+        end = time.time()
+        if(end-start > 10):
+            print(end-start)
+            print(receivedPacket)
+            break
+
 def initSetup():
     beetle = btle.Peripheral(BEETLEMAC1)
     beetle_delegate = Delegate(BEETLEMAC1)
@@ -43,6 +60,8 @@ def initSetup():
 
 if __name__ == '__main__':
     handShakeFlag = {BEETLEMAC1: False}
+    missPakcet = 0
+    receivedPacket = 0
     receivedData = bytes()
     beetle = initSetup()
     handShake(beetle)
