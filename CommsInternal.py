841150779604c8719d028a18092d3e10f462d939
tatayu <e0323328@u.nus.edu>
2021-02-25 14:16:35 +0800

    added multithreading

--- CommsInternal.py
+++ CommsInternal.py
@@ -1,27 +1,34 @@
 from bluepy import btle
 from bluepy.btle import BTLEException
+from datetime import datetime
 import struct
 import time
+import threading
 
-BEETLEMAC1 = '80:30:DC:D9:23:1E'
+BEETLEMAC1 = '80:30:DC:E9:1C:2F'
+BEETLEMAC2 = '80:30:DC:D9:1C:60'
 
 class Delegate(btle.DefaultDelegate):
-    def __init__(self, MAC):
+    def __init__(self, BEETLEMAC):
         btle.DefaultDelegate.__init__(self)
+        self.BEETLEMAC = BEETLEMAC
     
     def handleNotification(self, cHandle, data):
         global receivedData
         global missPakcet
         global receivedPacket
         if(data == b'A'): #handshake packet
-            handShakeFlag[BEETLEMAC1]= True
+            handShakeFlag[self.BEETLEMAC]= True
 
         elif (b'}' in data): #detect the end of a packet
             try:
                 receivedData += data[0:data.index(b'}')+1]
-                unpackedData = struct.unpack('<c6Hc', receivedData)
+                unpackedData = struct.unpack('<cI6Hc', receivedData)
                 receivedPacket += 1
-                print(receivedPacket)
+                #print(unpackedData)
+                #print('receive:',receivedPacket)
+                dataBuffer[self.BEETLEMAC] = unpackedData
+                print(self.BEETLEMAC, dataBuffer[self.BEETLEMAC])
                 receivedData = bytes() #reset
                 receivedData += data[data.index(b'}')+1:len(data)]
         
@@ -34,22 +41,47 @@ class Delegate(btle.DefaultDelegate):
         else:
             receivedData += data
 
-def handShake(beetle):
+class beetleThread (threading.Thread):
+    def __init__(self, threadID, BEETLEMAC):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.BEETLEMAC = BEETLEMAC
+    
+    def run(self):
+        beetle = initSetup(self.BEETLEMAC)
+        handShake(beetle, self.BEETLEMAC)
+        getData(beetle, self.BEETLEMAC)
+
+def handShake(beetle, BEETLEMAC):
     charac= beetle.getCharacteristics(uuid = 'dfb1')[0]
-    charac.write(bytes('H'), withResponse=False) #send handshake packet
+    charac.write(bytes('H', 'ISO 8859-1'), withResponse=False) #send handshake packet
     beetle.waitForNotifications(2)
 
-    if(handShakeFlag[BEETLEMAC1] == True): 
-        charac.write(bytes('A'), withResponse=False)
-        
-def reconnect(beetle):
+    if(handShakeFlag[BEETLEMAC] == True): 
+        charac.write(bytes('A', 'ISO 8859-1'), withResponse=False)
+
+'''       
+def sendTimestamp(beetle):
+    charac= beetle.getCharacteristics(uuid = 'dfb1')[0]
+    charac.write(bytes('T', 'ISO 8859-1'), withResponse=False)
+    beetle.waitForNotifications(2)
+
+    if(timestampFlag[BEETLEMAC1] == True):
+        timestamp = datetime.now().time()
+        print(timestamp)
+        #str(timestamp)
+        charac.write(bytes(str(timestamp), 'ISO 8859-1'), withResponse = False)
+        #beetle.waitForNotifications(2)
+'''
+
+def reconnect(beetle, BEETLEMAC):
     success = False
     tryConnect = 0
     while(tryConnect < 2 and success == False):
         try:
             print('Reconnecting...')
             beetle.disconnect()
-            beetle.connect(BEETLEMAC1)
+            beetle.connect(BEETLEMAC)
             success = True
 
         except BTLEException:
@@ -57,11 +89,11 @@ def reconnect(beetle):
             tryConnect += 1
     
     if(success == True):
-        handShake(beetle)
+        handShake(beetle, BEETLEMAC)
     
     return success
 
-def getData(beetle):
+def getData(beetle, BEETLEMAC):
     global receivedPacket
     #start = time.time()
     while(1):
@@ -70,7 +102,7 @@ def getData(beetle):
         
         except BTLEException:
             print('Device disconneted!')
-            reconnect(beetle)
+            reconnect(beetle, BEETLEMAC)
 
         '''
         end = time.time()
@@ -80,20 +112,23 @@ def getData(beetle):
             break
         '''
 
-def initSetup():
-    beetle = btle.Peripheral(BEETLEMAC1)
-    beetle_delegate = Delegate(BEETLEMAC1)
+def initSetup(BEETLEMAC):
+    beetle = btle.Peripheral(BEETLEMAC)
+    beetle_delegate = Delegate(BEETLEMAC)
     beetle.withDelegate(beetle_delegate)
     return beetle
 
 if __name__ == '__main__':
-    handShakeFlag = {BEETLEMAC1: False}
+    #print(datetime.now().time())
+    handShakeFlag = {BEETLEMAC1: False, BEETLEMAC2: False}
+    dataBuffer = {BEETLEMAC1: "", BEETLEMAC2: ""}
     missPakcet = 0
     receivedPacket = 0
     receivedData = bytes()
-    beetle = initSetup()
-    handShake(beetle)
-    getData(beetle)
+    thread1 = beetleThread(1, BEETLEMAC1)
+    thread2 = beetleThread(2, BEETLEMAC2)
+    thread1.start()
+    thread2.start()
     
     
 
